92ebc8ce91c0c761b98e87579ce53371865280f1
Andrew Collier <andrew@fathomdata.dev>
2021-07-25 17:14:00 +0300

    feat: Working version of incident_details()

--- R/traffic.R
+++ R/traffic.R
@@ -1,17 +1,72 @@
-#' Title
+URL_TRAFFIC_INCIDENT <- 'https://api.tomtom.com/traffic/services/5/incidentDetails'
+FIELDS <- '{incidents{type,geometry{type,coordinates},properties{id,iconCategory,magnitudeOfDelay,events{description,code},startTime,endTime,from,to,length,delay,roadNumbers,aci{probabilityOfOccurrence,numberOfReports,lastReportTime}}}}'
+
+# More documentation (with numerous other options):
+#
+# - https://developer.tomtom.com/traffic-api/traffic-api-documentation
+# - https://developer.tomtom.com/content/traffic-api-explorer
+
+#' Traffic incidents
+#'
+#' @param left Left edge of bounding box.
+#' @param bottom  Bottom edge of bounding box.
+#' @param right  Right edge of bounding box.
+#' @param top  Top edge of bounding box.
 #'
 #' @return
 #' @export
 #'
 #' @examples
-# incident_details <- function() {
-#   response = GET(
-#     'https://api.tomtom.com/traffic/services/5/incidentDetails',
-#     query = list(
-#       bbox = '4.8854592519716675,52.36934334773164,4.897883244144765,52.37496348620152',
-#       fields = '{incidents{type,geometry{type,coordinates},properties{id,iconCategory,magnitudeOfDelay,events{description,code},startTime,endTime,from,to,length,delay,roadNumbers,aci{probabilityOfOccurrence,numberOfReports,lastReportTime}}}}'
-#     )
-#   )
-#
-#   content(response)
-# }
+#' incidents <- incident_details(23.4, 37.9, 24.0, 38.2)
+incident_details <- function(left, bottom, right, top) {
+  bbox <- paste(c(left, bottom, right, top), collapse = ",")
+
+  response = tomtom:::GET(
+    URL_TRAFFIC_INCIDENT,
+    query = list(
+      bbox = bbox,
+      fields = FIELDS
+    )
+  )
+
+  traffic <- content(response)
+
+  unpack_incident <- function(incident) {
+    properties <- incident$properties
+    # Handle multiple events.
+    properties$events <- properties$events %>% map_dfr(identity)
+    properties <- properties %>% unlist(recursive = FALSE)
+    # Set up list columns.
+    properties$events.description = paste(properties$events.description, collapse = "; ")
+    properties$events.code = paste(properties$events.code, collapse = "; ")
+
+    properties <- as.data.frame(properties)
+
+    points <- sapply(incident$geometry$coordinates, c) %>%
+      t() %>%
+      data.frame() %>%
+      setNames(c("lon", "lat")) %>%
+      mutate_all(unlist)
+
+    tibble(properties, points = list(points))
+  }
+
+  map_dfr(traffic$incidents, unpack_incident) %>%
+    rename(
+      begin = startTime,
+      end = endTime
+    ) %>%
+    select(-id, -iconCategory, -magnitudeOfDelay) %>%
+    rename_at(
+      vars(starts_with("events")),
+      ~ sub("^.*\\.", "", .)
+    ) %>%
+    mutate(
+      length = as.numeric(length),
+      incident = row_number(),
+      description = factor(description)
+    ) %>%
+    select(
+      incident, begin, end, description, from, to, length, points
+    )
+}
