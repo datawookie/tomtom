65c2b05523282774cb02461da22404e509dbe3f7
Andrew Collier <andrew@fathomdata.dev>
2021-07-25 19:05:54 +0300

    fix: Remove {logger} dependency

--- R/tomtom.R
+++ R/tomtom.R
@@ -2,7 +2,6 @@
 #' @import purrr
 #' @import tidyr
 #' @import httr
-#' @import logger
 NULL
 
 PKG_VERSION <- utils::packageDescription('tomtom')$Version
@@ -17,7 +16,6 @@ PKG_VERSION <- utils::packageDescription('tomtom')$Version
 #' @examples
 #' set_api_key("USf9S3AINkT4MhQZfz66ogYa3ZEGwPzZ")
 set_api_key <- function(api_key) {
-  log_debug("Setting API key: {api_key}.")
   assign("api_key", api_key, envir = cache)
 }
 
